fb32b3cbecdd93683c7e80bd2c34a8bc108c00fd
Yana Roman <roman.yanochka@yandex.ru>
2021-09-23 14:09:16 +0300

    Адаптив

--- js/script.js
+++ js/script.js
@@ -1,63 +1,28 @@
-const invitationButton = document.querySelectorAll('.invitation__button');
-const popup = document.querySelector('.popup');
-const closeButton = popup.querySelector('.popup__close');
+const questionsButton = document.querySelectorAll('.questions__button');
+const questionsParagraph = document.querySelector('.questions__paragraph');
 const formElement = document.querySelector('.form');
 const formAdd = document.querySelector('#form-add');
-const buttonElement = formAdd.querySelector('.form__submit');
+const buttonElement = document.querySelector('.form__submit');
 
-// Открытие попап 
-function openPopup () {
-  resetFormAdd(formAdd);
-  buttonElement.setAttribute('disabled', true);
-  buttonElement.classList.add('.form__submit_inactive');
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePopupOnEsc);
-  popup.addEventListener('click', closePopupOnOverlay);
-};
-
-// Закрытие попапов
-function closePopup() {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupOnEsc);
-  popup.removeEventListener('click', closePopupOnOverlay);
+// функция открытия/закрытия ответа
+function activeParagraph () {
+  questionsParagraph.classList.toggle('questions__paragraph-active');
 }
-
-// закрытие попап кликом на оверлей
-function closePopupOnOverlay(evt) {
-  if (evt.target.classList.contains('popup_opened')) {
-    const popupOpened = document.querySelector('.popup_opened');
-    closePopup(popupOpened);
-  }
-}
-
-// закрытие попап нажатием на esc
-function closePopupOnEsc(evt) {
-  if (evt.key === 'Escape') {
-    const popupOpened = document.querySelector('.popup_opened');
-    closePopup(popupOpened);
-  }
-}
-
+Array.from(document.querySelectorAll(".questions__item")).forEach(e=>{e.addEventListener("click",(function(){
+  this.classList.toggle("questions__item-active");
+  let e=this.querySelector(".questions__paragraph");
+  e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"}))});
 // функция очистки формы 
 function resetFormAdd(formAdd) {
-  formAdd.reset();
+  formElement.reset();
 }
 
-// Открытие и закрытие попапа
-for (var a = 0; a < invitationButton.length; a++) {    
-  invitationButton[a].addEventListener('click',function(){
-    openPopup()
-  });
-}
-closeButton.addEventListener('click', () => closePopup());
 
 // submit
 const submitEditProfileForm = evt => {
   evt.preventDefault();
   buttonElement.setAttribute('disabled', true);
   buttonElement.classList.add('.form__submit_inactive');
-  resetFormAdd(formAdd);
-  closePopup();
+  resetFormAdd();
 }
 formElement.addEventListener('submit', submitEditProfileForm);
-
