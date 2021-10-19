5813bbca932606485de06760316b94d5db55e784
Yana Roman <roman.yanochka@yandex.ru>
2021-10-19 22:52:38 +0300

    попап записи, страница спасибо

--- js/script.js
+++ js/script.js
@@ -22,13 +22,13 @@ function resetFormAddfooter() {
   formAddfooter.reset();
 }
 
-
 // submit
 const submitEditProfileForm = evt => {
   evt.preventDefault();
   buttonElement.setAttribute('disabled', true);
   buttonElement.classList.add('.form__submit_inactive');
   resetFormAdd(formAdd);
+  openPopup();
 }
 
 const submitEditProfileFormFooter = evt => {
@@ -36,6 +36,44 @@ const submitEditProfileFormFooter = evt => {
   buttonElementfooter.setAttribute('disabled', true);
   buttonElementfooter.classList.add('.form__submit_inactive');
   resetFormAddfooter();
+  openPopup();
 }
 formElement.addEventListener('submit', submitEditProfileForm);
 formAddfooter.addEventListener('submit', submitEditProfileFormFooter);
+
+const invitationButton = document.querySelectorAll('.invitation__button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close');
+
+// Открытие попап 
+function openPopup () {
+  popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupOnEsc);
+  popup.addEventListener('click', closePopupOnOverlay);
+};
+
+// Закрытие попапов
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupOnEsc);
+  popup.removeEventListener('click', closePopupOnOverlay);
+}
+
+// закрытие попап кликом на оверлей
+function closePopupOnOverlay(evt) {
+  if (evt.target.classList.contains('popup_opened')) {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }
+}
+
+// закрытие попап нажатием на esc
+function closePopupOnEsc(evt) {
+  if (evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }
+}
+
+// закрытие попапа на x
+closeButton.addEventListener('click', () => closePopup());
